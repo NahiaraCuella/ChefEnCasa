c3c997337c3e4f296c35631c18df3981ac2e91a4
Abril Mosquera <lagartosautos@gmail.com>
2024-09-12 11:57:18 -0300

    Agregado y editados de tablas

--- scheema.sql
+++ scheema.sql
@@ -1,58 +1,78 @@
-CREATE Usuario {
+CREATE  USER 'AbrilMosquera'@'%' IDENTIFIED BY 'abril17';
+GRANT ALL PRIVILEGES ON chefencasa.* TO 'AbrilMosquera'@'%';
+SHOW DATABASES;
+
+USE chefencasa;
+
+CREATE TABLE usuario(
+   id INT  PRIMARY KEY AUTO_INCREMENT,
    user VARCHAR(50),
    contrasenia VARCHAR(50),
    email VARCHAR(50),
    idioma VARCHAR(50),
-   ID INT PRIMARY KEY AUTO_INCREMENT,
-   id_pais INT FOREIGN KEY
-}
+   pais_id INT,
+   FOREIGN KEY (pais_id) REFERENCES pais(id)
+)
 
-CREATE Pais{
+CREATE TABLE pais(
     id INT PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(50)
-}
+)
 
-CREATE Recetas {
+CREATE TABLE coccion(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50)
+)
+
+CREATE TABLE dietas (
+	nombre_de_recetas VARCHAR(50),
+	id INT PRIMARY KEY AUTO_INCREMENT
+)
+
+CREATE TABLE momentos_dias (
+    momentos_del_dias VARCHAR(50),
+    id INT PRIMARY KEY AUTO_INCREMENT
+)
+
+CREATE TABLE ingredientes (
+   ID INT PRIMARY KEY AUTO_INCREMENT,
+   tipo_de_ingrediente VARCHAR(50)
+)
+
+CREATE TABLE recetas(
     cantidad_de_personas INT,
     paso_a_paso TEXT,
     tipo_de_dificultad CHAR(3),
-    id_user INT FOREIGN KEY,
     id INT PRIMARY KEY AUTO_INCREMENT,
-    id_pais INT FOREIGN KEY,
-    id_ingredientes INT FOREIGN KEY,
-    id_cocciones INT FOREIGN KEY,
-    id_momentoDia INT FOREIGN KEY,
-    id_dietas INT FOREIGN KEY
-}
+    pais_id INT,
+    user_id INT,
+    ingredientes_id INT,
+    cocciones_id INT,
+    momentoDia_id INT,
+    dietas_id INT,
+    FOREIGN KEY (pais_id) REFERENCES pais(id),
+    FOREIGN KEY (user_id) REFERENCES usuario(id),
+    FOREIGN KEY (ingredientes_id) REFERENCES ingredientes(ID),
+    FOREIGN KEY (cocciones_id) REFERENCES coccion(id),
+    FOREIGN KEY (momentoDia_id) REFERENCES momentos_dias(id),
+    FOREIGN KEY (dietas_id) REFERENCES dietas(id)
+)
 
-CREATE TABLE Coccion{
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(50),
-}
-
-CREATE Ingredientes {
-   ID INT PRIMARY KEY AUTO_INCREMENT,
-   tipo de ingrediente VARCHAR(50)
-}
-
-CREATE TABLE Medidas {
+CREATE TABLE medidas (
     id INT PRIMARY KEY,
     recetas VARCHAR(50),
-    id_recetas INT FOREIGN KEY
-}
+    recetas_id INT,
+    FOREIGN KEY (recetas_id) REFERENCES recetas(id)
+)
 
-CREATE rec_ing{
+CREATE TABLE rec_ing(
     id  INT PRIMARY KEY AUTO_INCREMENT,
-    id_recetas INT FOREIGN KEY,
-    id_ingredientes INT FOREIGN KEY
-}
+    recetas_id INT,
+    ingredientes_id INT,
+    FOREIGN KEY (recetas_id) REFERENCES recetas(id),
+    FOREIGN KEY (ingredientes_id) REFERENCES ingredientes(ID)
+)
 
-CREATE Dietas {
-	nombre_de_recetas VARCHAR(50),
-	id INT PRIMARY KEY AUTO_INCREMENT
-}
-
-CREATE momentos_dias {
-    momentos_del_dias VARCHAR(50),
-    id INT PRIMARY KEY AUTO_INCREMENT
-}
+DROP TABLE momentos_dias;
+DESCRIBE pais;
+SHOW TABLES;
