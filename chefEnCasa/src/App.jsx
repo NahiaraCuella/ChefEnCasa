67c453cd2155b4d7b51aca8465266a2a0e82f2af
Abril Mosquera <lagrartosautos@gmail.com>
2024-10-09 16:51:50 -0300

    empece el header

--- chefEnCasa/src/App.jsx
+++ chefEnCasa/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './components/card'
+import Header from './components/header'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Card/>
+      <Header/>
     </>
   )
 }
