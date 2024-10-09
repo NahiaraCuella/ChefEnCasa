1b303c0b9761fbf5b9408f63d4eb5b48653d3558
Abril Mosquera <lagartosautos@gmail.com>
2024-10-09 11:56:37 -0300

    detalle de usuario, falta probarlo

--- app.py
+++ app.py
@@ -5,18 +5,16 @@ db_file = "chefencasa.db"
 
 app = Flask(__name__)
 
-
-
-@app.route('/usuarios')
-def usuario():
-
 # Conectar a la base de datos
-    conn = mysql.connector.connect(
+conn = mysql.connector.connect(
     host='10.9.120.5',
     user='chefencasa',
     password='chefencasa111',
     database='chefencasa'
 )
+
+@app.route('/usuarios')
+def usuario():
     cursor = conn.cursor(dictionary=True)
      # Consultar datos
     cursor.execute('SELECT * FROM usuario')
@@ -28,3 +26,16 @@ def usuario():
     
     # Devolver los resultados en formato JSON
     return jsonify(rows)
+
+@app.route('/usuarios/<int:id>')
+def detalle_usuario(id):
+    cursor = conn.cursor(dictionary=True)
+    cursor.row_factory = dict.factory
+    
+    user = "SELECT pais FROM pais_id = ?"
+    result = cursor.excuete(user, (id,))
+    pais = result.fetchall()
+    
+    conn.close()
+    result = {"usuario": usuario, "pais": pais}
+    return jsonify(result)
