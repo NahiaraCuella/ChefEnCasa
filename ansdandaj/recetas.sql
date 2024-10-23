e0e5befbe7b3137b98cd3e8a87a600505f2af44f
valentinadutoya <laravalentinadutoya@gmail.com>
2024-10-23 16:46:21 -0300

    cambiar todo como corresponde

--- ansdandaj/recetas.sql
+++ ansdandaj/recetas.sql
@@ -1,49 +1,17 @@
 INSERT INTO recetas (
-    user_id,
+     user_id,
     nombre_receta,
-    cantidad_personas,
+    porciones,
     paso_a_paso,
     tiempo_coccion,
-    tipo_dificultad,
-    ingredientes,
-    pais_id
-) 
+    tipo_dificultad, 
+    ingredientes_id,
+    pais_id,
+    cocciones_id,
+    dietas_id,
+    rec_ing
+    
+)
 VALUES(
-   (1,"Albondigas",4,
-   "1-Coloque la carne molida de res y cerdo en un tazón grande. Agregue la cebolla, el ajo,                             		cebolla larga, pimentón rojo, masarepa, huevo, sal y pimienta.
-    	 Mezclar bien con las manos.
-	2-Formar las albóndigas y colocarlos en una bandeja.
-	3-Colocar el aceite en una cacerola grande a fuego medio. Saltear las albóndigas hasta que 								estén bien doradas por todos lados, unos 7 minutos en total.
-    	 Retire las albóndigas a un plato cubierto con toallas de papel.
-	4-Para hacer la salsa: Espolvorear la harina y remover con una cuchara de madera para disolver en la grasa. 
-  	 Verter el caldo de res, y revolver con una cuchara de madera. Cocine a fuego lento y revuelva hasta que el líquido 	  se reduzca y la salsa se empieza a formar. Sazone con sal y pimienta.
-	5-Añadir el hogao y cocine por 10 minutos, revolviendo con frecuencia. Vuelva a poner las albóndigas en la salsa. 		 Cocine a fuego lento hasta que la salsa espese ligeramente y las albóndigas se cocinen, alrededor de 15 a 20 			minutos.
-	6-Espolvorear con el cilantro y sirva sobre arroz blanco.","tarda entre 30min o 45 min","facil",
-    "½ libra carne molida de cerdo
-	 ½ libra de carne molida de res
-	 1 huevo grande
-	 ½ cucharadita de comino molido
-	 ¼ de taza de harina de maíz precocida Masarepa
- 	 1 diente de ajo picado
-	 ¼ de taza de cebolla picada
-	 1 cebolla verde picada
-	 ¼ de taza de pimentón rojo
-	 Sal y pimienta
-	 2 cucharadas de aceite para cocinar a las albóndigas",1),(1,"Empanadas Saltenas de carne,4","1-Pela y pica en cubos la cebolla. Por otro lado, lava muy bien y pica la cebolla de verdeo. Reserva por separado las hojas, que incorporaremos crudas a último momento, 2- Corta la carne en pequeños cubos, reserva en heladera.3-Lava y pela la papa, córtala en pequeños cubos. Luego, hierve en abundante agua y sal a gusto. No debes cocinarlas del todo, es decir, deben quedar un poquito duras, ya que durante la cocción de la empanda, se cocinarán un poco más. Por lo tanto es preferible que las retires cuando aún estén un poquito duras.4-Lleva al fuego un recipiente con la grasa. Una vez que se derrita y se caliente, cocina allí la cebolla hasta que se pongan transparente.5-Añade los condimentos, revuelve bien y luego incorpora la carne. Cocina por aproximadamente 15 minutos. Puedes agregar un poco del caldo de cocción de la papa para ayudar a la ebullición.6-Una vez terminada la cocción, retira del fuego y agrega la papa. Revuelve, deja enfriar y guarda en heladera como mínimo un par de horas (ideal si puedes hacerlo el día anterior).7-Antes de comenzar a armar las empanadas, enciende el horno para precalentar al máximo. Luego, agrega al relleno de empanadas salteñas las hojas cortadas que reservamos de la cebolla de verdeo y el huevo duro picado. Rellena cada disco de empanada, luego une y realiza el repulgue.8-Distribuye las empanadas en una asadera aceitada y cocina en horno precalentado a 250 °C por alrededor de 15 minutos o hasta que estén doradas.","2h 30m ","facil","12 discos de masa para empanadas
- 500 gramos de carne (bola de lomo, nalga o cuadrada)
- 1 cebolla grande
- 1 cebolla de verdeo
- 1 papa
- 1 huevo duro
- 2 cucharadas soperas de grasa de pella
- sal, pimentón dulce, comino y ají molido a gusto",1),(3,"Pizza de arroz",4,"Para empezar con la preparación de la pizza de arroz cocido, cocina en abundante agua hirviendo por el tiempo indicado en el paquete, el cual suele ser unos 8 a 12 minutos. En este caso utilizamos arroz largo fino, pero puedes usar doble carolina para una consistencia más homogénea o bien arroz yamaní si prefieres aportar más fibra a tu pizza. También podrías usar arroz que te haya sobrado de otra preparación.2-Una vez cocido, deberás colarlo y dejar enfriar. Luego, mézclalo con el huevo, los condimentos y el almidón de maíz. Si lo prefieres, procesa los granos de arroz para una consistencia más homogénea.3-Calienta el horno a temperatura máxima, es decir, 200-220 ºC. Cuando alcance esa temperatura, unta una asadera con aceite y llévala unos 2 o 3 minutos al horno. Con cuidado de no quemarte, retira y agrega la preparación de arroz con huevo y almidón. Proporciona forma redondeada y cocínala por unos 20 minutos o hasta que esté seco y ligeramente dorado.4
-Pasado ese tiempo, retira tu pizza de arroz del horno, úntala con la salsa y agrega los cubos de queso. Llévala al horno nuevamente por unos 15 minutos para que el queso se derrita y la salsa se caliente.4-
-Pasado ese tiempo, retira tu pizza de arroz del horno, úntala con la salsa y agrega los cubos de queso. Llévala al horno nuevamente por unos 15 minutos para que el queso se derrita y la salsa se caliente.5-
-Sirve tu pizza de arroz. ¡A comer! Cuéntanos si la pruebas y comparte la fotografía del resultado final.","45m","facil"," ½ taza de arroz cocido (87½ gramos)
- 1 huevo
- 2 cucharadas soperas de maicena
- 1 cucharada postre de mostaza
- 60 gramos de queso fresco
- 4 aceitunas
- 2 cucharadas soperas de salsa de tomate",1)
+   (1,"albondigas",4,"Picamos el ajo, la albahaca y la cebolla y se la agregamos a la carne junto con el huevo y el pan rallado.Mezclamos hasta tener un bollo y dejamos reposar mientras 	preparamos la salsa. Sofreír la cebolla y el morrón a fuego bajo, para preparar las albóndigas.Empezamos a armar las bolitas, pasamos por una fina capa de pan rallado y vamos colocando en la sarténDoramos todos sus lados. Agregamos el vino (opcional) y una vez que se haya evaporado el alcohol, agregamos el puré de tomate."," 40min ","facil",5,1,3,7,"",)
 )
