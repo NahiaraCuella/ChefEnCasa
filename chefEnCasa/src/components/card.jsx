1f673b0130cf5395d901f64587fcd6d521d5d724
belenn chijo <belennnc25026@gmail.com>
2024-11-06 11:14:56 -0300

    Agregue las tarjetas

--- chefEnCasa/src/components/card.jsx
+++ chefEnCasa/src/components/card.jsx
@@ -1,35 +1,23 @@
-import React from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./card.css";
 
 const RecipeSlider = () => {
   const recipes = [
-    { id: 1, img: 'h', title: 'Receta 1' },
-    { id: 2, img: 'url_de_la_imagen_2', title: 'Receta 2' },
-    { id: 3, img: 'url_de_la_imagen_3', title: 'Receta 3' },
-    // Agrega más recetas aquí
+    { id: 1, img: "./public/h.webp", title: "Hotcakes" },
+    { id: 2, img: "./public/f.jpg", title: "Fideos con tuco" },
+    { id: 3, img: "./public/p.avif", title: "Fideos con salsa y salchicha" },
   ];
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
-
   return (
-    <div>
-      <h2>Recetas</h2>
-      <Slider {...settings}>
-        {recipes.map(recipe => (
-          <div key={recipe.id}>
-            <img src={recipe.img} alt={recipe.title} style={{ width: '100%', height: 'auto' }} />
-            <h3>{recipe.title}</h3>
-          </div>
-        ))}
-      </Slider>
+    <div className="image-container">
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          <img src={recipe.img} alt={recipe.title} className="image" />
+          <h3>{recipe.title}</h3>
+        </div>
+      ))}
     </div>
   );
 };
