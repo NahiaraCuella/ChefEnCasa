257c4f3873910cefbebcf501490d0293c58d63c6
belen <belennnc25026@gmail.com>
2024-11-07 09:31:01 -0300

    header

--- chefEnCasa/src/components/pagina.jsx
+++ chefEnCasa/src/components/pagina.jsx
@@ -1,23 +1,24 @@
 import "./pagina.css";
 
+
 export default function Boton() {
-  return (
-    <div className="caja">
-      <div className="alineamiento-botones">
-        <div className="alineamiento-boton-login">
-          <button id="login-btn">👤</button>
-        </div>
-        <div className="alineamiento-boton-pais">
-          <button id="country-btn">🇦🇷</button>
-        </div>
-      </div>
-      <header className="alineamiento_header">
-        <h1 className="titulo">Chef En Casa</h1>
-        <div>
-          <input className="busqueda" type="text" placeholder="buscar..." />
-          <button>🍳</button>
-        </div>
-      </header>
-    </div>
-  );
+ return (
+   <div className="caja">
+     <div className="alineamiento-botones">
+       <div className="alineamiento-boton-login">
+         <button id="login-btn">👤</button>
+       </div>
+       <div className="alineamiento-boton-pais">
+         <button id="country-btn">🇦🇷</button>
+       </div>
+     </div>
+     <header className="alineamiento_header">
+       <h1 className="titulo">Chef En Casa</h1>
+       <div>
+         <input className="busqueda" type="text" placeholder="buscar..." />
+         <button>🍳</button>
+       </div>
+     </header>
+   </div>
+ );
 }
